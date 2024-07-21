3c3d82b940c32fb4183c35af9330ba96eaa28d9b
Mohammad Talim <talimmohammad116@gmail.com>
2024-07-21 22:58:59 +0530

    fix: add the direct url instead of env

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,10 +2,11 @@ import { Referral } from "@/app/types";
 import axios from "axios";
 
 export async function postReferral(referralData: Referral) {
-  const apiUrl = process.env.API_URL;
-
   try {
-    const response = await axios.post(apiUrl!, referralData);
+    const response = await axios.post(
+      "https://accredian-backend-task-qqpw.onrender.com/referral",
+      referralData
+    );
     console.log("Referral submitted successfully:", response.data);
   } catch (error) {
     console.error("Error submitting referral:", error);
