76d88fa4318003e15f270db9818ddcb71e2e8070
Mohammad Talim <talimmohammad116@gmail.com>
2024-07-21 22:38:09 +0530

    add the deployed backend api

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,11 +2,14 @@ import { Referral } from "@/app/types";
 import axios from "axios";
 
 export async function postReferral(referralData: Referral) {
+  const apiUrl = process.env.API_URL;
+
+  if (!apiUrl) {
+    throw new Error("API URL is missing from the environment variables!");
+  }
+
   try {
-    const response = await axios.post(
-      "http://localhost:8000/referral",
-      referralData
-    );
+    const response = await axios.post(apiUrl, referralData);
     console.log("Referral submitted successfully:", response.data);
   } catch (error) {
     console.error("Error submitting referral:", error);
