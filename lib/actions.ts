80efb1fdda1d793abd12d428647856f51fea1bc1
Mohammad Talim <talimmohammad116@gmail.com>
2024-07-21 22:52:51 +0530

    fix: remove check for apiUrl

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,12 +4,8 @@ import axios from "axios";
 export async function postReferral(referralData: Referral) {
   const apiUrl = process.env.API_URL;
 
-  if (!apiUrl) {
-    throw new Error("API URL is missing from the environment variables!");
-  }
-
   try {
-    const response = await axios.post(apiUrl, referralData);
+    const response = await axios.post(apiUrl!, referralData);
     console.log("Referral submitted successfully:", response.data);
   } catch (error) {
     console.error("Error submitting referral:", error);
